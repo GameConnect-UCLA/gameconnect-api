e540a88bf59238c0490103dd9f906eb6d67d5ad5
joseAndresAlv <1001.27524130.ucla@gmail.com>
2026-07-12 22:52:55 -0400

    feat(password): implement reset password functionality with validation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,11 +6,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { EmailService } from '../email/email.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { EmailModule } from '../email/email.module'; // Importa tu módulo global de emails
 
 @Module({
-  imports: [JwtModule.register({}), PassportModule, CacheModule.register({ ttl: 900000, })],
-  providers: [AuthService, JwtStrategy, JwtAuthGuard, EmailService],
+  imports: [
+    PrismaModule, 
+    EmailModule, // Se agrega a los imports organizados
+    JwtModule.register({}), 
+    PassportModule, 
+    CacheModule.register({ ttl: 900000 })
+  ],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard], // Limpiamos EmailService de aquí
   controllers: [AuthController],
   exports: [JwtAuthGuard],
 })
